9eab39013996bd13b36676b58e12a8100ba059ef
Michael Fournier <michaeljfournier5@gmail.com>
2022-02-24 10:06:40 -0500

    commit tree ecd32b00d6e80c63c2aa1aeb9fdb056c38c57ea1

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    const copyOfCats = [...cats, "Broom"];
+    return copyOfCats;
+} 
+
+function prependCat(name){
+    const copy1OfCats = ["Arnold", ...cats];
+    return copy1OfCats;
+}
+
+function removeLastCat(){
+    const copy2OfCats = cats.slice(0,-1)
+    return copy2OfCats;
+}
+
+function removeFirstCat(){
+    const copy3OfCats = cats.slice(1)
+    return copy3OfCats;
+}
